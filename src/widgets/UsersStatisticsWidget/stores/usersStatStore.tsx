51197634fbc3c23fd170c8cf21e596c3368bedda
Roman Bulhakau <bulgakovra.w@gmail.com>
2024-11-25 18:53:25 +0300

    Fix store

--- src/widgets/UsersStatisticsWidget/stores/usersStatStore.tsx
+++ src/widgets/UsersStatisticsWidget/stores/usersStatStore.tsx
@@ -1,37 +1,35 @@
-import { create } from "zustand";
-import zukeeper from "zukeeper";
-import { immer } from "zustand/middleware/immer";
+import zukeeper from 'zukeeper'
+import { create } from 'zustand'
+import { immer } from 'zustand/middleware/immer'
 
-import { DatasetModel } from "../models";
+import { DatasetModel } from '../models'
 
 interface UsersStatStore {
-  dataset: DatasetModel[];
-  addUser: () => void;
+	dataset: DatasetModel[]
+	addUser: () => void
 }
 
-const usersStatStore = create<UsersStatStore, [["zustand/immer", never]]>(
-  immer(
-    zukeeper((set: (arg0: (state: UsersStatStore) => void) => void) => ({
-      dataset: [],
-      addUser: () =>
-        set((state: { dataset: { users: number; date: string }[] }) => {
-          const currentDate = new Date().toDateString();
-          const findDateIndex = state.dataset.findIndex(
-            (el: { date: string }) => el.date === currentDate
-          );
-          if (findDateIndex !== -1) {
-            const findDate = state.dataset[findDateIndex];
-            const newAmount = findDate.users + 1;
-            findDate.users = newAmount;
-            state.dataset[findDateIndex] = findDate;
-          } else {
-            state.dataset.push({ users: 1, date: currentDate });
-          }
-        }),
-    }))
-  )
-);
+const usersStatStore = create<UsersStatStore, [['zustand/immer', never]]>(
+	immer(
+		zukeeper((set: (arg0: (state: UsersStatStore) => void) => void) => ({
+			dataset: [],
+			addUser: () =>
+				set((state: { dataset: { users: number; date: string }[] }) => {
+					const currentDate = new Date().toDateString()
+					const findDateIndex = state.dataset.findIndex(
+						(el: { date: string }) => el.date === currentDate
+					)
+					if (findDateIndex !== -1) {
+						const findDate = state.dataset[findDateIndex]
+						const newAmount = findDate.users + 1
+						findDate.users = newAmount
+						state.dataset[findDateIndex] = findDate
+					} else {
+						state.dataset.push({ users: 1, date: currentDate })
+					}
+				}),
+		}))
+	)
+)
 
-window.store = usersStatStore;
-
-export default usersStatStore;
+export default usersStatStore
